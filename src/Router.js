5a5683c3034138391bd44d7c0c2e0a0c3d76d95f
Liew <soonhen18@gmail.com>
2017-10-14 17:58:34 +0800

    Accidentally removed the "auth" scene from Router.js

--- src/Router.js
+++ src/Router.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Scene, Router } from 'react-native-router-flux';
 import LandingPage from './Containers/Auth/LandingPage';
 import LoginPage from './Containers/Auth/LoginPage';
-import SignUpPage from './Containers/Auth/SignUpPage';
+import CustomerSignUpPage from './Containers/Auth/CustomerSignUpPage';
+import VendorSignUpPage from './Containers/Auth/VendorSignUpPage';
 import HomePage from './Containers/Home/HomePage';
 
 const RouterComponent = () => {
   return (
     <Router>
+      <Scene key="auth" navigationBarStyle={styles.authNavBarStyle} >
           <Scene key="landingPage" component={LandingPage} initial hideNavBar />
           <Scene
             key="loginPage"
@@ -15,7 +17,9 @@ const RouterComponent = () => {
             hideNavBar={false}
             titleStyle={styles.titleStyle} title={'E - R E N O'}
           />
-          <Scene key="signUpPage" component={SignUpPage} title={"Sign up"} />
+          <Scene key="customerSignUpPage" component={CustomerSignUpPage} title={"Sign up"} />
+          <Scene key="vendorSignUpPage" component={VendorSignUpPage} title={"Sign up"} />
+        </Scene>
 
         <Scene key="home">
           <Scene key="homePage" component={HomePage} initial />
