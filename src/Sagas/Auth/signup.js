d470faa3aa848c393fbfc6c46e1ff434080f103b
chenchuen <kevinleechenchuen@gmail.com>
2017-10-18 16:14:30 +0800

    Add signup saga

--- src/Sagas/Auth/signup.js
+++ src/Sagas/Auth/signup.js
@@ -5,34 +5,68 @@ import firebase from 'firebase';
 import ReduxActions from '../../Redux/Actions';
 import Types from '../../Redux/Auth/types';
 
-export function* watchSignUpUser() {
+export function* watchUserSignUp() {
   while (true) {
-    const { data } = yield take(Types.AUTH_SIGNUP_USER);
-    yield call(handleSignUpUser, data);
+    const { userData } = yield take(Types.AUTH_SIGN_UP_ATTEMPT);
+    yield call(handleSignUp, userData);
   }
 }
 
-export function handleSignUpUser(data) {
+export function* handleSignUp(data) {
+  if (data.userType === 'customer') {
+    yield call(handleCustomerSignUp, data);
+  } else if (data.userType === 'vendor') {
+    yield call(handleVendorSignUp, data);
+  }
+}
+
+export function handleCustomerSignUp(data) {
   firebase.auth().createUserWithEmailAndPassword(data.email, data.password)
     .then((user) => {
-      handleUserSignUpSuccess(user, data);
+      handleCustomerSignUpSuccess(user, data);//If success store other details
     })
     .catch((error) => {
-      handleUserSignUpFail(error);
+      handleSignUpFail(error);//If fail dispatch error msg
     });
 }
 
-
-export function* handleUserSignUpSuccess(data) {
-  const currentUser = firebase.auth();
-  firebase.database().ref(`Users/${currentUser.uid}/user`)
-  .push({ data.Name, data.PhoneNumber, data.Location, data.Email, data.Checkbox });
-
-
-  yield put(ReduxActions.userSignUpSuccess());
+export function handleVendorSignUp(data) {
+  firebase.auth().createUserWithEmailAndPassword(data.email, data.password)
+    .then((user) => {
+      handleVendorSignUpSuccess(user, data);//If success store other details
+    })
+    .catch((error) => {
+      handleSignUpFail(error);//If fail dispatch error msg
+    });
 }
 
-export function* handleUserSignUpFail(error) {
+export function* handleCustomerSignUpSuccess(data) {
+  const currentUser = firebase.auth();
+  const newUser = {
+    name: data.name,
+    userType: data.userType,
+    phonoNo: data.phoneNo,
+    address: data.address,
+    email: data.email,
+    checked: true,
+  };
+  console.log(newUser);
+  firebase.database().ref(`Users/customer/${currentUser.uid}`)
+    .push(newUser);
+
+  yield put(ReduxActions.authUserSignUpSuccess());
+}
+
+export function* handleVendorSignUpSuccess(data) {
+  const currentUser = firebase.auth();
+//`Users/vendor/${currentUser.uid}`
+
+  //LEE : pass in sign up success data, to be set in redux state,
+  //should be the same data as the sign in data
+  yield put(ReduxActions.authUserSignUpSuccess());
+}
+
+export function* handleSignUpFail(error) {
   console.log(error);
-  yield put(ReduxActions.userSignUpFail(error));
+  yield put(ReduxActions.authUserSignUpFail(error));
 }
