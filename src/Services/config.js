6107adac03aa54a038e1d369df81c711f86ea89c
chenchuen <kevinleechenchuen@gmail.com>
2018-01-31 15:27:38 +0800

    Added payment gateway link to config

--- src/Services/config.js
+++ src/Services/config.js
@@ -1,6 +1,6 @@
 export default {
   getFullParametersDomain: 'https://us-central1-aladdinapp-942fe.cloudfunctions.net/getFullParameters',
-
+  getPaymentGatewayDomain: 'https://test2pay.ghl.com/IPGSG/Payment.aspx',
   reduxLoggerEnabled: false,
 
   googleGeocoderAPI: 'AIzaSyDiQJNqCwJXrP4yp8MB-5xnxbCEV4oyRt0',
