71e72e0d34429ee4e6c078c01c45ea75d29e1331
Kevin Lee <kevinleechenchuen@gmail.com>
2018-05-07 12:57:41 +0800

    Added fcm to Android

--- android/app/src/main/java/com/ereno/MainApplication.java
+++ android/app/src/main/java/com/ereno/MainApplication.java
@@ -1,8 +1,9 @@
-package com.aladdin;
+package com.ereno;
 
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.evollu.react.fcm.FIRMessagingPackage;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.toast.RCTToastPackage;
 import com.rnfs.RNFSPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new FIRMessagingPackage(),
             new RCTCameraPackage(),
             new RCTToastPackage(),
             new RNFSPackage(),
