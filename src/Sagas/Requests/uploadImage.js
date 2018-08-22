edd63eb9db08de6c46965f6a14e632d1a92f0459
Lee Chen Chuen <kevinleechenchuen@gmail.com>
2018-08-22 16:36:39 +0800

    fixed uploadImage firebase error

--- src/Sagas/Requests/uploadImage.js
+++ src/Sagas/Requests/uploadImage.js
@@ -1,11 +1,15 @@
 import { take, call, put } from 'redux-saga/effects';
 import { Platform } from 'react-native';
+
 import firebase from 'firebase';
 import RNFetchBlob from 'react-native-fetch-blob';
 
 import Types from '../../Redux/Requests/types';
 
 import { showErrorToast } from '../../Services/helpers';
+
+const React = require('react-native'),
+window = global || window;
 
 export function* watchUploadImage() {
   while (true) {
@@ -21,16 +25,15 @@ export function* imageUploaderHandler(imageUrl, trxId) {
     yield call(uploadImage, properUrl, trxId);
   } catch (error) {
     console.log(error);
-
     showErrorToast("Something went wrong. Image not attached");
   }
 }
 
 export function uploadImage(uploadUri, trxid) {
-  console.log(uploadUri);
-  console.log(trxid);
   const Blob = RNFetchBlob.polyfill.Blob;
   const fs = RNFetchBlob.fs;
+  window.XMLHttpRequest = RNFetchBlob.polyfill.XMLHttpRequest;
+  window.Blob = Blob;
   const mime = 'application/octet-stream';
 
   return new Promise((resolve, reject) => {
@@ -40,23 +43,20 @@ export function uploadImage(uploadUri, trxid) {
 
     fs.readFile(uploadUri, 'base64')
     .then((data) => {
-      console.log(data);
       return Blob.build(data, { type: `${mime};BASE64` });
     })
     .then((blob) => {
       uploadBlob = blob;
-      return imageRef.put(blob._ref, blob, { contentType: mime });
+      return imageRef.put(blob, { contentType: mime });
     })
-    .then(() => {
+    .then((snapshot) => {
       uploadBlob.close();
-      return imageRef.getDownloadUrl();
+      return snapshot.downloadURL;
     })
     .then((url) => {
-      console.log(url);
       resolve(url);
     })
     .catch((error) => {
-      console.log(error);
       reject(error);
     });
   });
